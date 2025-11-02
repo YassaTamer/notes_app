568ee5a1555846d343927207ff08e12e972b20e0
YassaShahat <yassashahat5@gmail.com>
2025-11-02 12:39:36 +0200

    Refactor CustomAppBar and CustomSearchIcon to accept dynamic title and icon parameters

--- lib/widgets/custom_icon.dart
+++ lib/widgets/custom_icon.dart
@@ -1,9 +1,8 @@
-
 import 'package:flutter/material.dart';
 
 class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
-
+  const CustomSearchIcon.CustomIcon({super.key, required this.icon});
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -13,7 +12,7 @@ class CustomSearchIcon extends StatelessWidget {
         color: Colors.white.withOpacity(.08),
         borderRadius: BorderRadius.circular(12),
       ),
-      child: Center(child: Icon(Icons.search, size: 32)),
+      child: Center(child: Icon(icon, size: 32)),
     );
   }
 }
--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_app_bar.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(children:const [SizedBox(height: 48), CustomAppBar(
+        title: 'Edit mode',
+        icon: Icons.check,
+      )]),
+    );
   }
 }
