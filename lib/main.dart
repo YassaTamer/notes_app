9c87aab298f40e8b6fbfbd21fde0e23977ce4800
YassaShahat <yassashahat5@gmail.com>
2025-11-03 12:22:48 +0200

    Fix Hive initialization and refactor note input form with validation

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
-  await Hive.initFlutter;
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,16 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint,  this.maxLines = 1 });
+  const CustomTextField({
+    super.key,
+    required this.hint,
+    this.maxLines = 1,
+    this.onSaved,
+  });
   final String hint;
   final int maxLines;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'Field is required ';
+        } else {
+          return null;
+        }
+      },
+      onSaved: onSaved,
       maxLines: maxLines,
       decoration: InputDecoration(
         hintText: hint,
-        
+
         hintStyle: TextStyle(color: kPrimaryColor),
         border: buildBorder(),
         enabledBorder: buildBorder(),
