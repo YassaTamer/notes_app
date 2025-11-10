69ad7a8fb8037351bb5bc11eee76afb8f19cd456
YassaShahat <yassashahat5@gmail.com>
2025-11-10 13:24:59 +0200

    Refactor: Enhance color selection in AddNoteForm by implementing ColorsListView and ColorItem for better user interaction

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -43,6 +43,8 @@ class _AddNoteFormState extends State<AddNoteForm> {
           ),
           const SizedBox(height: 32),
           ColorsListView(),
+          const SizedBox(height: 32),
+
           BlocBuilder<AddNoteCubit, AddNoteState>(
             builder: (context, state) {
               return CustomBotton(
@@ -75,4 +77,3 @@ class _AddNoteFormState extends State<AddNoteForm> {
     );
   }
 }
-
--- lib/widgets/colors_list_view.dart
+++ lib/widgets/colors_list_view.dart
@@ -1,20 +1,49 @@
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/add_note_form.dart';
 import 'package:notes_app/widgets/color_item.dart';
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = [
+    Colors.amber,
+    Colors.blueGrey,
+    Colors.cyanAccent,
+    Colors.cyan,
+    Colors.lightGreen,
+    Colors.orange,
+    Colors.teal,
+    Colors.yellow,
+    Colors.purpleAccent,
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 48,
       child: ListView.builder(
-        itemCount: 8,
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return ColorItem();
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: colors[index],
+                isActive: currentIndex == index,
+              ),
+            ),
+          );
         },
       ),
     );
